64e2cf2903de2f21f03f01c413f95bc34c7bc383
DEUSYEET <47902658+DEUSYEET@users.noreply.github.com>
2020-10-19 15:15:44 -0600

    BUILD WAS fricking IN THE GITIGNORE

---
+++ build/precache-manifest.56b67d45669651b9fd2df5b849b3e828.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d801a6d9161ec649807414b2172ec00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd00c8059a23862fad66",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4075a9c0c5822b7ab525",
+    "url": "/static/js/2.d0009750.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.d0009750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd00c8059a23862fad66",
+    "url": "/static/js/main.d2c2e751.chunk.js"
+  },
+  {
+    "revision": "0fec8ff6b6b8b756b70b",
+    "url": "/static/js/runtime-main.ab52c7f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
